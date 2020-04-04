f5de42e3cf475a1bedd7eaea7bef6b79eeb3c65e
Adallah Daknache <abdallah_daknache@hotmail.com>
2020-04-04 09:29:31 +0300

    Updates

---
+++ precache-manifest.7200cf5e5af7dff88c0ed743358547b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b56711429836b569c448203356ef87",
+    "url": "/tellmeajoke/index.html"
+  },
+  {
+    "revision": "3bfbc2493202c87017c9",
+    "url": "/tellmeajoke/static/css/main.7dc09c13.chunk.css"
+  },
+  {
+    "revision": "befa3b10d0e905dca9c9",
+    "url": "/tellmeajoke/static/js/2.307585c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tellmeajoke/static/js/2.307585c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfbc2493202c87017c9",
+    "url": "/tellmeajoke/static/js/main.6c31f4ef.chunk.js"
+  },
+  {
+    "revision": "d5dccc00707fdd284f2a",
+    "url": "/tellmeajoke/static/js/runtime-main.862a1ef4.js"
+  },
+  {
+    "revision": "120d0ecaa2b4e53ddf64d033c1573e61",
+    "url": "/tellmeajoke/static/media/happy.120d0eca.png"
+  }
+]);
